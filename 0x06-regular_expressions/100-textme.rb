361e798419d4749d038ebdf349120f42f26184f8
OduogoWalter <oduogowalter@gmail.com>
2024-03-18 21:36:09 +0300

    100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,3 +1,19 @@
 #!/usr/bin/env ruby
-# A regular expression that is matches a given pattern
-puts ARGV[0].scan(/\[from:(.*?)\]\s\[to:(.*?)\]\s\[flags:(.*?)\]/).join(',')
+
+# Read input from command line argument
+input = ARGV[0]
+
+# Regular expression to extract sender, receiver, and flags
+regex = /\[from:(?<sender>[^\]]+)\] \[to:(?<receiver>[^\]]+)\] \[flags:(?<flags>[^\]]+)\]/
+
+# Match the regular expression against the input
+matches = input.match(regex)
+
+# Output sender, receiver, and flags if match is found
+if matches
+  sender = matches[:sender]
+  receiver = matches[:receiver]
+  flags = matches[:flags]
+  puts "#{sender},#{receiver},#{flags}"
+end
+

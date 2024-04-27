cddb3501cbe0ff0904160ad9706f617b3d00091f
OduogoWalter <oduogowalter@gmail.com>
2024-04-28 00:30:11 +0300

    100-count.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,7 +7,7 @@ of all hot articles for a given subreddit
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=None):
+def recurse(subreddit, hot_list=[]):
     """
     Recursively retrieves the titles of all hot articles for a given subreddit
 
@@ -15,8 +15,6 @@ def recurse(subreddit, hot_list=[], after=None):
         subreddit (str): The name of the subreddit
         hot_list (list): List to store the titles
         of hot articles (default is empty list)
-        after (str): Identifier for the last post
-        in the current page (default is None)
 
     Returns:
         list or None: List containing titles of hot articles,
@@ -28,12 +26,9 @@ def recurse(subreddit, hot_list=[], after=None):
     # Reddit API endpoint for retrieving hot posts in a subreddit
     url = f'https://www.reddit.com/r/{subreddit}/hot.json'
 
-    # Parameters for pagination
-    params = {'after': after} if after else {}
-
     # Send GET request to the API endpoint
     try:
-        response = requests.get(url, headers=headers, params=params)
+        response = requests.get(url, headers=headers)
 
         # Check if the request was successful
         if response.status_code == 200:
@@ -46,7 +41,7 @@ def recurse(subreddit, hot_list=[], after=None):
             # Check if there are more pages of hot posts
             after = data['data']['after']
             if after:
-                return recurse(subreddit, hot_list=hot_list, after=after)
+                return recurse(subreddit, hot_list=hot_list)
             else:
                 return hot_list
         elif response.status_code == 404:

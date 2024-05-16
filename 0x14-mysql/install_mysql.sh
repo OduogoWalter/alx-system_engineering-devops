3623e743c4814dae10485050f37ffd2227a0bd36
OduogoWalter <oduogowalter@gmail.com>
2024-05-16 14:04:35 +0000

    install_mysql.sh

--- 0x14-mysql/install_mysql.sh
+++ 0x14-mysql/install_mysql.sh
@@ -1,20 +1,14 @@
 #!/usr/bin/env bash
-# This script installs MySQL 5.7 on an Ubuntu 16.04 server
+# This script installs MySQL 5.7.x on web-01 and web-02 servers
 
 # Update package lists
-sudo apt-get update
+sudo apt update
 
-# Install the required package for adding a repository
-sudo apt-get install -y software-properties-common
+# Install MySQL 5.7.x
+sudo apt install mysql-server -y
 
-# Add the MySQL repository
-sudo add-apt-repository -y "deb http://repo.mysql.com/apt/ubuntu/ $(lsb_release -sc) mysql-5.7"
-
-# Update package lists after adding MySQL repository
-sudo apt-get update
-
-# Install MySQL 5.7
-sudo apt-get install -y mysql-server-5.7
-
-# Check MySQL version to confirm the installation
+# Verify MySQL installation
 mysql --version
+
+# Check MySQL service status
+sudo systemctl status mysql

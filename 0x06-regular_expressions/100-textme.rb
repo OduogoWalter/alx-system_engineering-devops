1e27b31ce718db0694004a92c9cb7bb4a8f0bef1
OduogoWalter <oduogowalter@gmail.com>
2024-05-17 12:15:44 +0300

    2-repetition_token_1.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,19 +1,4 @@
 #!/usr/bin/env ruby
+# This script outputs script should output: [SENDER],[RECEIVER],[FLAGS]
 
-# Read input from command line argument
-input = ARGV[0]
-
-# Regular expression to extract sender, receiver, and flags
-regex = /\[from:(?<sender>[^\]]+)\] \[to:(?<receiver>[^\]]+)\] \[flags:(?<flags>[^\]]+)\]/
-
-# Match the regular expression against the input
-matches = input.match(regex)
-
-# Output sender, receiver, and flags if match is found
-if matches
-  sender = matches[:sender]
-  receiver = matches[:receiver]
-  flags = matches[:flags]
-  puts "#{sender},#{receiver},#{flags}"
-end
-
+puts ARGV[0].scan(/\[from:(\+?\w*)\]\s\[to:(\+?\w*)\]\s\[flags:(\S*)\]/).join(',')

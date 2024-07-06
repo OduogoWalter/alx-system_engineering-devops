7a9e8cb6b0c4bea72bb01e8dbd9619237e7f5b93
OduogoWalter <oduogowalter@gmail.com>
2024-07-06 09:29:55 +0300

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""Module to query the Reddit API and return the number of subscribers"""
+"""
+This module provides functionality to interact with the Reddit API.
+It contains function to retrieve the no of subscribers for a given subreddit.
+"""
 import requests
 
 
@@ -7,6 +10,12 @@ def number_of_subscribers(subreddit):
     """
     Queries the Reddit API and returns the number of subscribers
     for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The number of subscribers if the subreddit is valid, 0 otherwise.
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'Custom User Agent 1.0'}
